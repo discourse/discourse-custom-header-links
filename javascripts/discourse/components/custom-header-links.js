803787ba74450f4d24a355f0a6ac1e1b1eeda8a6
OsamaSayegh <asooomaasoooma90@gmail.com>
2024-04-25 02:03:58 +0300

    Revert "Revert "DEV: Migrate custom header links to new objects setting type (#53)""
    This reverts commit a178fdd2f7dc7a3488890198ad13f2d7da7cb4c1.

--- javascripts/discourse/components/custom-header-links.js
+++ javascripts/discourse/components/custom-header-links.js
@@ -7,16 +7,14 @@ export default class CustomHeaderLinks extends Component {
   }
 
   get links() {
-    return settings.custom_header_links.split("|").reduce((result, item) => {
-      let [
-        linkText,
-        linkTitle,
-        linkHref,
-        device,
-        target = "",
-        keepOnScroll,
-        locale,
-      ] = item.split(",").map((s) => s.trim());
+    return settings.custom_header_links.reduce((result, link) => {
+      const linkText = link.text;
+      const linkTitle = link.title;
+      const linkHref = link.url;
+      const target = link.target;
+      const hideOnScroll = link.hide_on_scroll;
+      const locale = link.locale;
+      const device = link.view;
 
       if (!linkText || (locale && document.documentElement.lang !== locale)) {
         return result;
@@ -32,8 +30,8 @@ export default class CustomHeaderLinks extends Component {
 
       result.push({
         device: `headerLink--${device}`,
-        keepOnScroll: `headerLink--${keepOnScroll}`,
-        locale: `headerLink--${locale}`,
+        hideOnScroll: `headerLink--${hideOnScroll}`,
+        locale: locale ? `headerLink--${locale}` : null,
         linkClass,
         anchorAttributes,
         linkText,
