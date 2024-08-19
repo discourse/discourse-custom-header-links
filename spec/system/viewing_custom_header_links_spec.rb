ec751e4631fc5e38f64dc6f010923fe017343526
Sérgio Saquetim <1108771+megothss@users.noreply.github.com>
2024-08-19 16:35:56 -0300

    DEV: Remove old header widget code (#66)

--- spec/system/viewing_custom_header_links_spec.rb
+++ spec/system/viewing_custom_header_links_spec.rb
@@ -40,71 +40,27 @@ RSpec.describe "Viewing Custom Header Links", system: true do
     theme.save!
   end
 
-  context "when glimmer headers are enabled" do
-    before do
-      if SiteSetting.respond_to?(:experimental_glimmer_header_groups)
-        SiteSetting.experimental_glimmer_header_groups = Group::AUTO_GROUPS[:everyone]
-      else
-        SiteSetting.glimmer_header_mode = "enabled"
-      end
-    end
+  it "should display the custom header links" do
+    visit("/")
 
-    it "should display the custom header links" do
-      visit("/")
+    expect(custom_header_link).to be_visible
 
-      expect(custom_header_link).to be_visible
+    expect(custom_header_link).to have_custom_header_link(
+      "External link",
+      href: "https://meta.discourse.org",
+      title: "This link will open in a new tab",
+    )
 
-      expect(custom_header_link).to have_custom_header_link(
-        "External link",
-        href: "https://meta.discourse.org",
-        title: "This link will open in a new tab",
-      )
+    expect(custom_header_link).to have_custom_header_link(
+      "Most Liked",
+      href: "/latest/?order=op_likes",
+      title: "Posts with the most amount of likes",
+    )
 
-      expect(custom_header_link).to have_custom_header_link(
-        "Most Liked",
-        href: "/latest/?order=op_likes",
-        title: "Posts with the most amount of likes",
-      )
-
-      expect(custom_header_link).to have_custom_header_link(
-        "Privacy",
-        href: "/privacy",
-        title: "Our Privacy Policy",
-      )
-    end
-  end
-
-  context "when glimmer headers are disabled" do
-    before do
-      if SiteSetting.respond_to?(:experimental_glimmer_header_groups)
-        SiteSetting.experimental_glimmer_header_groups = nil
-      else
-        SiteSetting.glimmer_header_mode = "disabled"
-      end
-    end
-
-    it "should display the custom header links" do
-      visit("/")
-
-      expect(custom_header_link).to be_visible
-
-      expect(custom_header_link).to have_custom_header_link(
-        "External link",
-        href: "https://meta.discourse.org",
-        title: "This link will open in a new tab",
-      )
-
-      expect(custom_header_link).to have_custom_header_link(
-        "Most Liked",
-        href: "/latest/?order=op_likes",
-        title: "Posts with the most amount of likes",
-      )
-
-      expect(custom_header_link).to have_custom_header_link(
-        "Privacy",
-        href: "/privacy",
-        title: "Our Privacy Policy",
-      )
-    end
+    expect(custom_header_link).to have_custom_header_link(
+      "Privacy",
+      href: "/privacy",
+      title: "Our Privacy Policy",
+    )
   end
 end
